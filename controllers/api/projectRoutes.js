f4a5f286bf9b59e0a6d1df31d39168d03ca9de71
Leo Wong <leoelicos@gmail.com>
2022-05-26 15:28:51 +1000

    Start code from BCS

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+
+router.get('/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id);
+
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // console.log(`projects = `, projects);
+
+    res.render('homepage', {
+      projects,
+
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const projectData = await Project.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Project, User } = require('../models');
+// const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // console.log(`projects = `, projects);
+
+    res.render('homepage', {
+      projects,
+
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
