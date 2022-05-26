0d1d38b2b8984dbee62a6fbf9806487e12b61956
Leo Wong <leoelicos@gmail.com>
2022-05-26 16:50:36 +1000

    Add GET route to /profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,6 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
+    // Get all projects and JOIN with user data
     const projectData = await Project.findAll({
       include: [
         {
@@ -13,11 +14,12 @@ router.get('/', async (req, res) => {
       ],
     });
 
+    // Serialize data so the template can read it
     const projects = projectData.map((project) => project.get({ plain: true }));
 
+    // Pass serialized data and session flag into template
     res.render('homepage', {
       projects,
-
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -27,17 +29,16 @@ router.get('/', async (req, res) => {
 
 router.get('/project/:id', async (req, res) => {
   try {
-    const data = await Project.findByPk(req.params.id, {
+    const projectData = await Project.findByPk(req.params.id, {
       include: [
         {
-          mode: User,
+          model: User,
           attributes: ['name'],
         },
       ],
     });
 
-    //serialize
-    const project = data.get({ plain: true });
+    const project = projectData.get({ plain: true });
 
     res.render('project', {
       ...project,
@@ -48,9 +49,30 @@ router.get('/project/:id', async (req, res) => {
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/profile');
     return;
   }
 
