e65dec2bba25b80fb10b2cd13da10e285dabd15d
Leo Wong <leoelicos@gmail.com>
2023-04-08 03:19:37 +1000

    Delete comment

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,9 @@
 const withAuth = (req, res, next) => {
-	// TODO: Add a comment describing the functionality of this if statement
-	// if user is not logged in, redirect to login page, otherwise go ahead
-	if (!req.session.logged_in) {
-		res.redirect('/login');
-	} else {
-		next();
-	}
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
 };
 
 module.exports = withAuth;
